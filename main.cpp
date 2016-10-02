67e33c6b4fe7ac920413beec4c994a5b9c3a216d
Dongliang "Larry" Yi <larry.ydl@gmail.com>
2016-10-01 20:04:57 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,192 @@
+//  Fibonacci
+//
+//  Created by Dongliang Yi on 9/21/16.
+//  Copyright © 2016 Dongliang Yi. All rights reserved.
+//
+
+#include <iostream>
+
+/*
+int solution(long N)
+{
+    // store some initialized data
+    unsigned long long x = 1, y = 1, a = 0, b = 1,t;
+    
+    // if N equals zeor, will return 0, otherwise will run below code
+    while (N>0)
+    {
+        //decide whether the last digit is zero or not
+        if (N & 1)
+        {
+            //Below equations are based on fabonacci identical equations, please refer to https://en.wikipedia.org/wiki/Fibonacci_number
+            t = a*x + b*y;
+            a = x*(b - a) + a*y;
+            b = t;
+        }
+        
+        // Below equations are also based on fabonacci identical equations, please refer to https://en.wikipedia.org/wiki/Fibonacci_number
+        t = 2 * x*y - x*x;
+        y = x*x + y*y;
+        x = t;
+        
+        // make sure all digits were visited
+        N >>= 1;
+    }
+    //find the last six digits
+    int last_6digits = a%1000000;
+    return last_6digits;
+    
+}
+
+
+int solution2(long N)
+{
+    // store some initialized data
+    unsigned long long x = 1, y = 1, a = 0, b = 1,t;
+    
+    // if N equals zeor, will return 0, otherwise will run below code
+    while (N>0)
+    {
+        //decide whether the last digit is zero or not
+        if (N & 1)
+        {
+            //Below equations are based on fabonacci identical equations, please refer to https://en.wikipedia.org/wiki/Fibonacci_number
+            t = (a*x%1000000 + b*y%1000000);
+            a = (x*b%1000000 - x*a%1000000 + a*y%1000000);
+            b = t;
+        }
+        
+        // Below equations are also based on fabonacci identical equations, please refer to https://en.wikipedia.org/wiki/Fibonacci_number
+        t = (2 * x*y%1000000 - x*x%1000000);
+        y = (x*x + y*y)%1000000;
+        x = t;
+        
+        // make sure all digits were visited
+        N >>= 1;
+    }
+    //find the last six digits
+    int last_6digits = a%1000000;
+    return last_6digits;
+    
+}*/
+
+// This function has the smallest running time, and it can calculate billinoth fabonacci number's last 6 digits.
+long long solution(int N)
+{
+    
+    //pleaser refer to https://ronzii.wordpress.com/2011/07/09/using-matrix-exponentiation-to-calculated-nth-fibonacci-number/
+    // I just module the intermedia result and in this way, we can alwasy get the right last 6 digits
+    long long fib[2][2]= {{1,1},{1,0}},ret[2][2]= {{1,0},{0,1}},tmp[2][2]= {{0,0},{0,0}};
+    int i,j,k;
+    while(N)
+    {
+        //Below equations are based on fabonacci identical equations, please refer to https://en.wikipedia.org/wiki/Fibonacci_number
+        if(N&1)
+        {
+            //memset(tmp,0,sizeof tmp);
+            tmp[0][0]= 0;
+            tmp[0][1]= 0;
+            tmp[1][0]= 0;
+            tmp[1][1]= 0;
+            
+            
+            
+            std::cout << tmp << "\n";
+            for(i=0; i<2; i++)
+                for(j=0; j<2; j++)
+                    for(k=0; k<2; k++)
+                        tmp[i][j]=(tmp[i][j]%1000000+ret[i][k]%1000000*fib[k][j])%1000000;
+            for(i=0; i<2; i++)
+                for(j=0; j<2; j++)
+                    ret[i][j]=tmp[i][j]%1000000;
+        }
+        
+        //Below equations are based on fabonacci identical equations, please refer to https://en.wikipedia.org/wiki/Fibonacci_number
+        tmp[0][0]= 0;
+        tmp[0][1]= 0;
+        tmp[1][0]= 0;
+        tmp[1][1]= 0;
+
+        for(i=0; i<2; i++)
+            for(j=0; j<2; j++)
+                for(k=0; k<2; k++)
+            tmp[i][j]=(tmp[i][j]%1000000+fib[i][k]%1000000*fib[k][j]%1000000);
+        
+        for(i=0; i<2; i++)
+            for(j=0; j<2; j++)
+                fib[i][j]=tmp[i][j]%1000000;
+        
+        N/=2;
+    }
+    return (ret[0][1])%1000000;
+}
+
+
+unsigned long long int Fib5(int index)
+{
+    if(index<1)
+    {
+        return-1;
+    }
+    
+    long long int a1=1,a2=1,a3=1;
+    for(int i=0;i<index-2;i++)
+    {
+        a3=a1+a2;
+        a1=a2;
+        a2=a3;
+    }
+    return a3;
+}
+
+unsigned long long Fibonacci(int n){
+    unsigned long long x = 1, y = 1, a = 0, b = 1,t;
+    while (n>0)
+    {
+        if (n & 1){
+            t = a*x + b*y;
+            a = x*(b - a) + a*y;
+            b = t;
+        }
+        t = 2 * x*y - x*x;
+        y = x*x + y*y;
+        x = t;
+        n >>= 1;
+    }
+    return a;
+}
+
+
+
+
+int main(int argc, const char * argv[])
+{
+    // insert code here...
+
+    clock_t start, finish;
+    double duration;
+    //double fibonacci;
+    //long int fibonacci;
+    
+    start = clock();
+    
+    //for(int i = 1; i<100; i++){
+    //fibonacci = Fibonacci(60);
+    //fibonacci = Fib5(100);
+    //}
+    
+    finish = clock();
+    
+    
+    duration = (double)(finish - start) / CLOCKS_PER_SEC;
+
+    long i = 10000;
+    
+    std::cout <<"Result: "<< Fibonacci(i) << "\n";
+    std::cout <<"Result: "<< Fib5(i) << "\n";
+    std::cout <<"Result: "<< solution(i) << "\n";
+    std::cout <<"Result: "<< solution2(i) << "\n";
+    //std::cout <<"Result: "<< fibonacci(i) << "\n";
+    //std::cout <<"Time: " << duration << "\n";
+    return 0;
+}
